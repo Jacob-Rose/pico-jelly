8d7c101bf6681f0c50a85d5de6bae39589f7af07
Jacob Rose <jacob.rose@corp.survios.com>
2022-05-17 22:56:00 -0700

    first commit

---
+++ jelly_main.ino
@@ -0,0 +1,158 @@
+#include <FastLED.h>
+
+#include "palettes.h"
+#include "utils.h"
+
+#define LEG_LED_COUNT 54
+#define HEAD_TOP_SIDE_LED_COUNT 30
+#define HEAD_BOTTOM_SIDE_LED_COUNT 26
+#define HEAD_LED_COUNT HEAD_TOP_SIDE_LED_COUNT + HEAD_BOTTOM_SIDE_LED_COUNT
+
+#define NUM_LEGS 12
+
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+
+#define UPDATES_PER_SECOND 60
+
+
+CRGB legLEDs[NUM_LEGS][LEG_LED_COUNT];
+CRGB headLEDs[3][HEAD_LED_COUNT];
+
+#pragma region Pins
+#define LEG1PIN 1
+#define LEG2PIN 2
+#define LEG3PIN 3
+#define LEG4PIN 4
+#define LEG5PIN 5
+#define LEG6PIN 6
+#define LEG7PIN 7
+#define LEG8PIN 8
+#define LEG9PIN 9
+#define LEG10PIN 10
+#define LEG11PIN 11
+#define LEG12PIN 12
+
+#define HEADSIDE1PIN 13
+#define HEADSIDE2PIN 14
+#define HEADSIDE3PIN 15
+#pragma endregion
+
+
+struct FPaletteCycleVars
+{
+  bool ShouldAutoSwapPalettes = true;
+  float TimeBetweenPaletteSwapping = 2.0f;
+
+  absolute_time_t TimeForNextPaletteSwap;
+} PaletteCycleVars;
+
+enum EPattern
+{
+  PaletteCycle,
+  HueCycle,
+};
+
+uint8_t indexOffset = 0;
+
+EPattern currentPattern = EPattern::PaletteCycle;
+
+
+CRGBPalette16 currentPalette = gp_ocean_foam;
+EPalette currentPaletteType = EPalette::OceanFoam;
+EPalette targetPaletteType = EPalette::OceanFoam;
+
+
+// DA JELLY TOTEM CODE
+
+void setup() 
+{
+  #pragma region Pins
+  FastLED.addLeds<LED_TYPE, LEG1PIN, COLOR_ORDER>(legLEDs[0], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG2PIN, COLOR_ORDER>(legLEDs[1], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG3PIN, COLOR_ORDER>(legLEDs[2], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG4PIN, COLOR_ORDER>(legLEDs[3], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG5PIN, COLOR_ORDER>(legLEDs[4], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG6PIN, COLOR_ORDER>(legLEDs[5], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG7PIN, COLOR_ORDER>(legLEDs[6], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG8PIN, COLOR_ORDER>(legLEDs[7], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG9PIN, COLOR_ORDER>(legLEDs[8], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG10PIN, COLOR_ORDER>(legLEDs[9], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG11PIN, COLOR_ORDER>(legLEDs[10], 0, LEG_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, LEG12PIN, COLOR_ORDER>(legLEDs[11], 0, LEG_LED_COUNT);
+
+  FastLED.addLeds<LED_TYPE, HEADSIDE1PIN, COLOR_ORDER>(headLEDs[0], 0, HEAD_TOP_SIDE_LED_COUNT + HEAD_BOTTOM_SIDE_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, HEADSIDE2PIN, COLOR_ORDER>(headLEDs[1], 0, HEAD_TOP_SIDE_LED_COUNT + HEAD_BOTTOM_SIDE_LED_COUNT);
+  FastLED.addLeds<LED_TYPE, HEADSIDE3PIN, COLOR_ORDER>(headLEDs[2], 0, HEAD_TOP_SIDE_LED_COUNT + HEAD_BOTTOM_SIDE_LED_COUNT);
+#pragma endregion
+
+  FastLED.setBrightness(64);
+
+  toggleOnboardLED(true);
+
+
+  PaletteCycleVars.TimeForNextPaletteSwap = make_timeout_time_ms(static_cast<uint32_t>(PaletteCycleVars.TimeBetweenPaletteSwapping * 1000));
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  switch(currentPattern)
+  {
+    case EPattern::HueCycle:
+      Update_HueCycle();
+      break;
+    case EPattern::PaletteCycle:
+      Update_PaletteCycle();
+      break;
+  }
+  indexOffset++;
+
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
+}
+
+void Update_PaletteCycle()
+{
+  for(int legIndex = 0; legIndex < NUM_LEGS; ++legIndex)
+  {
+    for(int ledIndex = 0; ledIndex < LEG_LED_COUNT; ++ledIndex)
+    {
+      legLEDs[legIndex][ledIndex] = ColorFromPalette( currentPalette, indexOffset + sin8(ledIndex*8), 255);
+    }
+  }
+
+  for(int legIndex = 0; legIndex < 3; ++legIndex)
+  {
+    for(int ledIndex = 0; ledIndex < HEAD_LED_COUNT; ++ledIndex)
+    {
+      headLEDs[legIndex][ledIndex] = ColorFromPalette( currentPalette, indexOffset + sin8(ledIndex*8), 255);
+    }
+  }
+
+
+}
+
+
+void Update_HueCycle()
+{
+  CRGB Color = CHSV(indexOffset, 255, 255);
+
+  for(int legIndex = 0; legIndex < NUM_LEGS; ++legIndex)
+  {
+    for(int ledIndex = 0; ledIndex < LEG_LED_COUNT; ++ledIndex)
+    {
+      legLEDs[legIndex][ledIndex] = Color;
+    }
+  }
+
+  for(int legIndex = 0; legIndex < 3; ++legIndex)
+  {
+    for(int ledIndex = 0; ledIndex < HEAD_LED_COUNT; ++ledIndex)
+    {
+      headLEDs[legIndex][ledIndex] = Color;
+    }
+  }
+}
---
+++ palettes.h
@@ -0,0 +1,47 @@
+#include <FastLED.h>
+
+enum EPalette
+{
+  OceanFoam,
+  CaliSunset,
+  Rainbow
+};
+
+// Gradient palette "bhw2_turq_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_turq.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( gp_ocean_foam ) {
+    0,   1, 33, 95,
+   38,   1,107, 37,
+   76,  42,255, 45,
+  127, 255,255, 45,
+};
+
+// Gradient palette "bhw1_01_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_01.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( gp_cali_sunset ) {
+    0, 227,101,  3,
+  117, 194, 18, 19,
+  255,  92,  8,192};
+
+
+void SetPaletteFromEnum(CRGBPalette16& OutPalette, EPalette PaletteRequested)
+{
+  switch(PaletteRequested)
+  {
+    case EPalette::OceanFoam:
+      OutPalette = gp_ocean_foam;
+      break;
+    case EPalette::CaliSunset:
+      OutPalette = gp_cali_sunset;
+      break;
+    case EPalette::Rainbow:
+      OutPalette = RainbowColors_p;
+      break;
+  }
+}
